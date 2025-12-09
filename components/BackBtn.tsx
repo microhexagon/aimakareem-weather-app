222928cea76e35fce238c8c751a60818756b1e26
aima kareem <amikareem921@gmail.com>
2025-12-09 18:35:56 +0500

    connect btn with homepage

--- components/BackBtn.tsx
+++ components/BackBtn.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function BackBtn() {
     return (
 
-        <Link href="\page.tsx">
+        <Link href="/">
             <button className=" bg-blue-500 text-white font-medium text-xl 
             rounded-2xl  h-12 w-40 flex items-center justify-center
             hover:bg-gray-700 hover:scale-105 transition-transform">
