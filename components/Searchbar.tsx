88663ac0c1d463c401389cad5ff1e6d28cf3bf4f
aima kareem <amikareem921@gmail.com>
2025-12-09 17:48:49 +0500

    functional searchbar

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,38 +1,39 @@
 'use client';
 import Link from "next/link"; 
-import { useState } from 'react';
 import { FaSearch } from "react-icons/fa";
 
+export default function SearchBar({ city, setCity }) {
 
-export default function WeatherPage() {
-const [city, setCity] = useState('');
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter' && city.trim() !== '') {
+            window.location.href = `/weather/${city}`;
+        }
+    };
 
-return (
-    <main className="">
-        <div className="flex flex-col items-center space-y-6">
-            <h2 className='font-medium text-2xl text-white'>
-                Weather Dashboard
-            </h2>
-
-          {/* Input + Button Row */}
-            <div className="flex items-center space-x-2">
-                <input
+    return (
+        <div className="flex items-center space-x-2 w-full">
+            <input
                 type="text"
                 value={city}
                 onChange={(e) => setCity(e.target.value)}
                 placeholder="Enter city name"
-                className='border border-blue-500 rounded-lg h-12 px-4 bg-gray-700 text-white focus:outline-none'
-                    />
-                <Link href={`/weather/${city}`}>
-                    <button 
-                        className="bg-blue-400 h-12 rounded-lg px-4 flex items-center gap-2 hover:bg-blue-500 hover:scale-105 transition-all duration-300 text-white"
-                    >
-                        <FaSearch />
-                        <span>Search</span>
-                    </button>
-                </Link>
-            </div>
+                className='flex-1 px-6 py-4 rounded-2xl bg-gray-800 
+                text-white border border-blue-400 focus:outline-none 
+                focus:border-blue-500 placeholder-gray-500'
+                onKeyPress={handleKeyPress}
+            />
+
+            <Link href={`/weather/${city}`}>
+                <button
+                    className=" bg-blue-500 text-white font-medium text-xl 
+                    rounded-2xl  px-6 py-4 flex items-center justify-center 
+                    hover:bg-gray-700 hover:scale-105 transition-transform"
+                >
+                    <FaSearch />
+                    Search
+                    
+                </button>
+            </Link>
         </div>
-    </main>
-);
+    );
 }
